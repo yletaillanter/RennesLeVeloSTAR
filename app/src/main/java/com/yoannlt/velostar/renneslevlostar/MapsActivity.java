19c24f90eccfe381426bd83eaf8bd9cf6ea5ec4f
yletaillanter <y.letaillanter@gmail.com>
2016-07-18 23:20:32 +0200

    Refactoring (remove locationlistener/manager)

--- app/src/main/java/com/yoannlt/velostar/renneslevlostar/MapsActivity.java
+++ app/src/main/java/com/yoannlt/velostar/renneslevlostar/MapsActivity.java
@@ -6,6 +6,8 @@ import android.content.pm.PackageManager;
 import android.location.Location;
 import android.location.LocationManager;
 import android.net.Uri;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
@@ -15,7 +17,11 @@ import android.widget.Toast;
 
 import com.google.android.gms.appindexing.Action;
 import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -26,13 +32,16 @@ import com.yoannlt.velostar.renneslevlostar.api.StarVelo;
 import com.yoannlt.velostar.renneslevlostar.model.Enregistrement;
 import com.yoannlt.velostar.renneslevlostar.model.JSONStationResponse;
 
+import java.util.List;
+import java.util.Map;
+
 import retrofit.RestAdapter;
 import rx.Observer;
 import rx.android.schedulers.AndroidSchedulers;
 
 import static com.google.android.gms.location.LocationServices.*;
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback, UpdateLocationCallback {
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback, GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener, LocationListener {
 
     private final static String LOG_TAG = MapsActivity.class.getSimpleName();
     private final static String ENDPOINT = "https://data.explore.star.fr/api/records/1.0/search";
@@ -40,42 +49,73 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     private final static int MY_LOCATION_REQUEST_CODE = 1;
     private final static long INTERVAL = 0;
 
-    private GoogleApiClient client;
+    private GoogleApiClient googleApiClient;
+    private LocationRequest locationRequest;
     private GoogleMap mMap;
-    private LocationManager locationManager;
     private Location mLastLocation;
     private StarVelo api;
+    private JSONStationResponse jsonStationResponse;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
 
+        //init the google API client
+        googleApiClient = new GoogleApiClient.Builder(this)
+                .addApi(LocationServices.API)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .build();
+
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+    }
 
-        // Get the locationmanager from the systemService
-        locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
 
-        MyLocListener myLocListener = new MyLocListener(this);
+        // Create the map
+        mMap = googleMap;
 
-        // Check if user authorized location
+        //Load the data
+        loadData();
+
         if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
-            //Update the location
-            locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, INTERVAL, 0, myLocListener);
+            // Activate user's position
+            mMap.setMyLocationEnabled(true);
+
+            //Test if lastLocation is known
+            if(mLastLocation != null) {
+                // Moce the camera to the user's position
+                LatLng currentPos = new LatLng(mLastLocation.getAltitude(), mLastLocation.getLongitude());
+                mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(currentPos, 13));
+            }
         } else {
-            // Get the location permission from user
-            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION},MY_LOCATION_REQUEST_CODE);
+            // Ask for permission
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, MY_LOCATION_REQUEST_CODE);
         }
+    }
 
-        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).addApi(API).build();
+    private void loadData() {
 
+        // Prepare the endpoint
         api = new RestAdapter.Builder()
                 .setEndpoint(ENDPOINT)
                 .build()
                 .create(StarVelo.class);
-        //RxJava
+
+        //RxJava part
         api.getAllStations()
                 .observeOn(AndroidSchedulers.mainThread())
                 .subscribe(new Observer<JSONStationResponse>() {
@@ -92,101 +132,94 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                     @Override
                     public void onNext(JSONStationResponse args) {
                         Log.d(LOG_TAG, args.toString());
-                        loadStationsOnMap(args);
-                        // Move the camera to the user's current location
-                        LatLng currentPos = new LatLng(mLastLocation.getAltitude(), mLastLocation.getLongitude());
-                        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(currentPos, 10));
+                        jsonStationResponse = args;
+                        loadStationsOnMap();
                     }
                 });
     }
 
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
-    @Override
-    public void onMapReady(GoogleMap googleMap) {
-        mMap = googleMap;
-
-
-        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
-                == PackageManager.PERMISSION_GRANTED) {
-            mMap.setMyLocationEnabled(true);
-            } else {
-            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION}, MY_LOCATION_REQUEST_CODE);
-        }
-
-    }
-
-    private void loadStationsOnMap(JSONStationResponse stationResponse) {
+    private void loadStationsOnMap() {
         LatLng latLng;
-        for (Enregistrement record : stationResponse.getRecords()) {
-            double distance = 0;
-            latLng = new LatLng(record.getFields().getCoordonnees()[0], record.getFields().getCoordonnees()[1]);
-            if (mLastLocation != null ) {
-                distance = distFrom(record.getFields().getCoordonnees()[0], record.getFields().getCoordonnees()[1], mLastLocation.getLatitude(), mLastLocation.getLongitude());
-            } else {
-                Log.d(LOG_TAG, "Location is null");
-            }
 
-            if (record.getFields().getEtat().equals(ETAT)) {
-                mMap.addMarker(new MarkerOptions().position(latLng).title("à " + distance + "m " + record.getFields().getNom() + " vélo(s) : " + record.getFields().getNombrevelosdisponibles() + " emplacement(s) : " + record.getFields().getNombreemplacementsdisponibles()));
-            } else {
-                mMap.addMarker(new MarkerOptions().position(latLng).title(record.getFields().getNom() + " En panne."));
+        if (jsonStationResponse != null) {
+            //mMap.clear();
+
+            // For each stations
+            for (Enregistrement record : jsonStationResponse.getRecords()) {
+                double distance = 0;
+                latLng = new LatLng(record.getFields().getCoordonnees()[0], record.getFields().getCoordonnees()[1]);
+                if (mLastLocation != null ) {
+                    distance = distFrom(record.getFields().getCoordonnees()[0], record.getFields().getCoordonnees()[1], mLastLocation.getLatitude(), mLastLocation.getLongitude());
+                } else {
+                    Log.d(LOG_TAG, "Location is null");
+                }
+
+                if (record.getFields().getEtat().equals(ETAT)) {
+                    MarkerOptions marker = new MarkerOptions().position(latLng).title("à " + distance + "m " + record.getFields().getNom() + " vélo(s) : " + record.getFields().getNombrevelosdisponibles() + " emplacement(s) : " + record.getFields().getNombreemplacementsdisponibles());
+                    mMap.addMarker(marker);
+                } else {
+                    MarkerOptions marker = new MarkerOptions().position(latLng).title("à " + distance + "m " + record.getFields().getNom() + " vélo(s) : " + record.getFields().getNombrevelosdisponibles() + " emplacement(s) : " + record.getFields().getNombreemplacementsdisponibles());
+                    mMap.addMarker(new MarkerOptions().position(latLng).title(record.getFields().getNom() + " En panne."));
+                }
+                mMap.moveCamera(CameraUpdateFactory.newLatLng(latLng));
             }
-            mMap.moveCamera(CameraUpdateFactory.newLatLng(latLng));
         }
     }
 
     @Override
     public void onStart() {
         super.onStart();
-
-        // ATTENTION: This was auto-generated to implement the App Indexing API.
-        // See https://g.co/AppIndexing/AndroidStudio for more information.
-        client.connect();
-        Action viewAction = Action.newAction(
-                Action.TYPE_VIEW, // TODO: choose an action type.
-                "Maps Page", // TODO: Define a title for the content shown.
-                // TODO: If you have web page content that matches this app activity's content,
-                // make sure this auto-generated web page URL is correct.
-                // Otherwise, set the URL to null.
-                Uri.parse("http://host/path"),
-                // TODO: Make sure this auto-generated app deep link URI is correct.
-                Uri.parse("android-app://com.yoannlt.velostar.renneslevlostar/http/host/path")
-        );
-        AppIndex.AppIndexApi.start(client, viewAction);
+        googleApiClient.connect();
     }
 
     @Override
     public void onStop() {
         super.onStop();
-
-        // ATTENTION: This was auto-generated to implement the App Indexing API.
-        // See https://g.co/AppIndexing/AndroidStudio for more information.
-        Action viewAction = Action.newAction(
-                Action.TYPE_VIEW, // TODO: choose an action type.
-                "Maps Page", // TODO: Define a title for the content shown.
-                // TODO: If you have web page content that matches this app activity's content,
-                // make sure this auto-generated web page URL is correct.
-                // Otherwise, set the URL to null.
-                Uri.parse("http://host/path"),
-                // TODO: Make sure this auto-generated app deep link URI is correct.
-                Uri.parse("android-app://com.yoannlt.velostar.renneslevlostar/http/host/path")
-        );
-        AppIndex.AppIndexApi.end(client, viewAction);
-        client.disconnect();
+        googleApiClient.disconnect();
     }
 
     @Override
-    public void updateLocation(Location location) {
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        if (requestCode == MY_LOCATION_REQUEST_CODE) {
+            if (permissions.length == 1 &&
+                    permissions[0] == Manifest.permission.ACCESS_FINE_LOCATION &&
+                    grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+            }
+        }
+    }
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+
+        //Init the location request
+        locationRequest = LocationRequest.create();
+        locationRequest.setPriority(LocationRequest.PRIORITY_BALANCED_POWER_ACCURACY);
+        locationRequest.setInterval(1000);
+
+        // Check if user authorized location
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            LocationServices.FusedLocationApi.requestLocationUpdates(googleApiClient, locationRequest, (LocationListener) this);
+        } else {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION},1);
+        }
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
         mLastLocation = location;
+        loadStationsOnMap();
         Log.e("location : ", location.getLatitude() + " / " + location.getLongitude());
+    }
+
+
+    @Override
+    public void onConnectionSuspended(int i) {
+        Log.d(LOG_TAG, "Suspended");
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+        Log.d(LOG_TAG, ""+connectionResult.describeContents());
     }
 
     /**
@@ -209,17 +242,4 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
         return dist;
     }
-
-    @Override
-    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
-        if (requestCode == MY_LOCATION_REQUEST_CODE) {
-            if (permissions.length == 1 &&
-                    permissions[0] == Manifest.permission.ACCESS_FINE_LOCATION &&
-                    grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-            } else {
-                Toast.makeText(getApplicationContext(), "Impossible d'obtenir la localisation", Toast.LENGTH_SHORT).show();
-            }
-        }
-    }
-
 }
